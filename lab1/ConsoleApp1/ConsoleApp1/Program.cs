f621f8c6f42b8e1264d12efcfa3e595b444355dc
Anastasia <simyeness@gmail.com>
2018-02-13 12:42:21 +0600

    lab1 final version

---
+++ lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                YearToCenturyConverter coverter = new YearToCenturyConverter();
+                Console.Write("Введите номер года: ");
+                string yearString = Console.ReadLine();
+                int cent = coverter.ConvertToCentury(yearString);
+                Console.WriteLine("Столетие: {0}", cent);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ lab1/ConsoleApp1/ConsoleApp1/YearToCenturyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class YearToCenturyConverter
+    {
+        public int ConvertToCentury(string yearString)
+        {
+            int yearInt = Convert.ToInt32(yearString);
+            int century = (yearInt - 1) / 100 + 1;
+            return century;
+        }
+    }
+}
