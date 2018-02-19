250f66945ebe009dc959a7d4d010db342f450dcd
Anastasia <simyeness@gmail.com>
2018-02-20 00:36:00 +0600

    lab2 version 1

---
+++ lab2/ConsoleApp1/ConsoleApp1/CountingOfRoundBrackets.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    public class CountingOfRoundBrackets
+    {
+        public int ToCountOfRoundBrackets(string textString)
+        {
+            int i = 0, error = 0, number = 0, answer;
+            while (i < textString.Length)
+            {
+                if (textString[i] == '(') number = number + 1;
+                if (textString[i] == ')') number = number - 1;
+                if (number < 0 && error == 0) error = i;
+                i++;
+            }
+            if (error != 0) answer = error + 1;
+            else if (number > 0) answer = -1;
+            else answer = 0;
+
+            return answer;
+        }
+    }
+}
---
+++ lab2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CountingOfRoundBrackets counting = new CountingOfRoundBrackets();
+            Console.Write("Введите ваш текст с круглыми скобками: ");
+            string textString = Console.ReadLine();
+            int count = counting.ToCountOfRoundBrackets(textString);
+            Console.WriteLine("Ответ: {0}", count);
+            if (count == 0) Console.WriteLine("Скобки расставлены правильно, каждой открывающей соответствует одна закрывающая");
+            if (count == -1) Console.WriteLine("Не хватает закрывающей скобки");
+            if (count > 0) Console.WriteLine("Ответ ({0}) равен номеру позиции, в которой расположена первая ошибочная закрывающая скобка", count);
+            Console.ReadLine();
+        }
+    }
+}
