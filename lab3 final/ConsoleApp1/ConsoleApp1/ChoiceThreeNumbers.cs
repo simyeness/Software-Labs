166ee19c0e7a17fb1f27afa213490b3c5bd787be
Anastasia <simyeness@gmail.com>
2018-02-28 18:10:45 +0600

    lab3 final version

---
+++ lab3 final/ConsoleApp1/ConsoleApp1/ChoiceThreeNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class ChoiceThreeNumbers
+    {
+        public int[] ToChoiceNumbers(int[] array)
+        {
+
+            int n1 = array[0]; int n2 = array[1]; int n3 = array[2];
+            if(array.Count(e => e > 0) >= 3)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (n1 < array[i])
+                    {
+                        n3 = n2;
+                        n2 = n1;
+                        n1 = array[i];
+                    }
+                }
+            }
+            
+
+            int negativeMax1 = array[0];
+            int negativeMax2 = array[1];
+
+            if (array.Count(e=> e < 0) >= 2)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (n1 > array[i])
+                    {
+                        negativeMax2 = negativeMax1;
+                        negativeMax1 = array[i];
+                    }
+                }
+            }
+
+            int[] answer = new int[3];
+            if (n1 * n2 * n3 > n1 * negativeMax1 * negativeMax2)
+            {
+                answer[0] = n1;
+                answer[1] = n2;
+                answer[2] = n3;
+            }
+            else
+            {
+                answer[0] = n1;
+                answer[1] = negativeMax1;
+                answer[2] = negativeMax2;
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ lab3 final/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            ChoiceThreeNumbers choice = new ChoiceThreeNumbers();
+
+            Console.Write("Введите количество элементов в массиве: ");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            int[] mass = new int[numbers];
+
+            
+
+            for (int y = 0; y < mass.Length; y++)
+            {
+                mass[y] = rand.Next(-100, 100);
+                Console.WriteLine($"{y+1} элемент = {mass[y]}");
+            }
+
+            int[] choiceNumbers = choice.ToChoiceNumbers(mass);
+
+            Console.WriteLine("\nЧисла, при которых произведение максимально:");
+
+            for (int h = 0; h < choiceNumbers.Length; h++)
+            {
+                Console.Write(choiceNumbers[h]+" ");
+            }
+
+            int answerFinal = choiceNumbers[0] * choiceNumbers[1] * choiceNumbers[2];
+            Console.WriteLine($"\nПроизведение: {answerFinal}");
+
+            Console.ReadLine();
+        }
+    }
+}
