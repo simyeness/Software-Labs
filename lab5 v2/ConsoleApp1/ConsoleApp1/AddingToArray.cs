050f9bdcff31e24fdf60f86961b43b22e284195a
Anastasia <simyeness@gmail.com>
2018-03-05 18:12:39 +0600

    lab5 version 2

---
+++ lab5 v2/ConsoleApp1/ConsoleApp1/AddingToArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+   public class AddingToArray
+    {
+        public List<AddingToArray> ToAdding(List<int> array)
+        {
+            var result = new List<AddingToArray>();
+            int sum = 0;
+            int number = 0;
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] > 0)
+                {
+                    sum += array[i];
+                    number++;
+                }
+            }
+            double arMean = Convert.ToDouble(sum / number);
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (Math.Abs(array[i]) > arMean)
+                {
+                    array.Insert(i + 1, 0);
+                }
+            }
+            Console.WriteLine($"Среднее арифметическое: {arMean}");
+            return result;
+        }
+    }
+}
