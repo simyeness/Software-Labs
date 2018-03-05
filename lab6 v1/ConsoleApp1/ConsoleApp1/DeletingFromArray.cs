fd6d04150ef25b560fa18cced28238a233afb979
Anastasia <simyeness@gmail.com>
2018-03-05 18:13:25 +0600

    lab6 version 1

---
+++ lab6 v1/ConsoleApp1/ConsoleApp1/DeletingFromArray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class DeletingFromArray
+    {
+
+        public List<DeletingFromArray> ToDeleting(List<int> array)
+        {
+            var result = new List<DeletingFromArray>();
+
+            for(int i = 0; i < array.Count - 1; i++)
+            {
+                    array.RemoveAll(x => x % 2 != 0);
+            }
+
+            return result;
+        }
+    }
+}
