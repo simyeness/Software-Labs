01ac626aebb9f3dc63ea251535b7fa616f357874
Anastasia <simyeness@gmail.com>
2018-03-05 17:07:13 +0600

    lab4 version 1

---
+++ lab4 v1/ConsoleApp1/ConsoleApp1/ChangeTwoMax.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    class ChangeTwoMax
+    {
+        public int[] ToChoiceTwoMax(int[] array)
+        {
+
+            int max1 = 0;
+            int max2 = 0;
+            int index1 = 0;
+            int index2 = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= max1)
+                {
+                    max1 = array[i];
+                    index1 = i;
+                }
+
+                else if (array[i] >= max2)
+                {
+                    max2 = array[i];
+                    index2 = i;
+                }
+            }
+            int temp = array[index1];
+
+            array[index1] = array[index2];
+            array[index2] = temp;
+
+            return array;
+        }
+    }
+}
---
+++ lab4 v1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите размер массива:");
+            int number = int.Parse(Console.ReadLine());
+
+            int[] array = new int[number]; 
+
+            Random rand = new Random();
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                array[j] = rand.Next(0, 50);
+            }
+
+            Console.WriteLine("Вывод исходного массива:");
+            foreach (int item in array)
+            {
+                Console.Write(item.ToString() + " ");  
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            ChangeTwoMax change = new ChangeTwoMax();
+
+            int[] twoMax = change.ToChoiceTwoMax(array);
+
+            Console.WriteLine("Вывод измененного массива:");
+
+            foreach (int item in array)
+            {
+                Console.Write(item.ToString() + " ");  
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
