ddf26452994e662fa6d07a01ae3970848edaf1c8
Anastasia <simyeness@gmail.com>
2018-03-05 17:08:02 +0600

    lab5 version 1

---
+++ lab5 v1/ConsoleApp1/ConsoleApp1/CalculationArithmeticMean.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+   public class CalculationArithmeticMean
+    {
+        public List<CalculationArithmeticMean> ArithmeticMean(List<int> array)
+        {
+            var result = new List<CalculationArithmeticMean>();
+            int sum = 0;
+            int number = 0;
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] > 0)
+                {
+                    sum += array[i];
+                    number++;
+                }
+            }
+            double arMean = Convert.ToDouble(sum / number);
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (Math.Abs(array[i]) > arMean)
+                {
+                    array.Insert(i + 1, 0);
+                }
+            }
+            Console.WriteLine($"Среднее арифметическое: {arMean}");
+            return result;
+        }
+    }
+}
---
+++ lab5 v1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            List<int> array = new List<int>() { 4, 5, 6, -4, -5, -12, -34, 3, 4 };
+
+            Console.WriteLine("Исходный массив: ");
+
+            foreach (int item in array)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            CalculationArithmeticMean output = new CalculationArithmeticMean();
+            List<CalculationArithmeticMean> calculation = output.ArithmeticMean(array: array);
+
+            Console.WriteLine();
+            Console.WriteLine("Результат: ");
+            foreach (int item in array)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
