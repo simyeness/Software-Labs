981796ccfa282de2b5a1ded2c72b09515220c438
Anastasia <simyeness@gmail.com>
2018-03-13 01:41:03 +0600

    lab7 version 1

---
+++ lab7 v1/ConsoleApp1/ConsoleApp1/FindOfElements.cs
@@ -0,0 +1,59 @@
+﻿namespace ConsoleApp1
+
+{
+    class FindOfElements
+    {
+        private int[,] array;
+
+        public FindOfElements(int[,] array)
+        {
+            this.array = array;
+        }
+
+        public int[,] Replacement(int[,] array)
+        {
+
+            int negative = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                if (array[array.GetLength(0) - 1, i] < 0)
+                {
+                    negative = i;
+                    break;
+                }
+
+                //if((array[array.GetLength(0) - 1, i] > 0) && negative > array[array.GetLength(0) - 1, i])
+                //{
+                //    negative = i;
+
+                //}
+            }
+
+            int positive = 0;
+
+            for (int i = array.GetLength(1) - 1; i >= 0; i--)
+            {
+                if (array[0, i] > 0)
+                {
+                    positive = i;
+                    break;
+                }
+
+                //if((array[0, i] < 0) && positive < array[0, i])
+                //{
+                //    positive = i;
+                //}
+            }
+
+
+            int element = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                element = array[i, positive];
+                array[i, positive] = array[i, negative];
+                array[i, negative] = element;
+            }
+            return array;
+        }
+    }
+}
---
+++ lab7 v1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Поменять местами столбец с первым отрицательным в последней
+строке и последним положительным в первой.*/
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Генерируемый начальный массив: ");
+            Random rand = new Random();
+            int n = 3;
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = rand.Next(-10, 10);
+                    Console.Write(String.Format("{0,3} ", array[i, j]));
+                }
+                Console.WriteLine("\t");
+            }
+
+            Console.WriteLine();
+            FindOfElements Array = new FindOfElements(array);
+            int[,] result = Array.Replacement(array);
+            Console.WriteLine("Массив после перестановки: ");
+            for (int i = 0; i < result.GetLength(1); i++)
+            {
+                for (int j = 0; j < result.GetLength(0); j++)
+                {
+                    Console.Write(String.Format("{0,3} ", result[i, j]));
+                }
+                Console.WriteLine("\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
