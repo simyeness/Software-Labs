1929da80d2fe6723d099365d5a88c7f42f191a13
Anastasia <simyeness@gmail.com>
2018-03-13 01:41:43 +0600

    lab9 version 1

---
+++ lab9 v1/ConsoleApp1/ConsoleApp1/Streamlining.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    class Streamlining
+    {
+        private int[,] array;
+        public Streamlining(int[,] array)
+        {
+            this.array = array;
+        }
+
+        public int[,] Replacement(int[,] array)
+        {
+            int element = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int x = 0; x < array.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < array.GetLength(1); y++)
+                        {
+                            if (array[i, j] > array[x, y])
+                            {
+                                element = array[i, j];
+                                array[i, j] = array[x, y];
+                                array[x, y] = element;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
