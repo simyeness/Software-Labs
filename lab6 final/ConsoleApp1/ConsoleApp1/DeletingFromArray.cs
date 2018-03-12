ef7f3def8895f40dadaa55311ec2fc2cdef87bb9
Anastasia <simyeness@gmail.com>
2018-03-13 01:40:05 +0600

    lab6 final version

---
+++ lab6 final/ConsoleApp1/ConsoleApp1/DeletingFromArray.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class DeletingFromArray
+    {
+
+        public List<DeletingFromArray> ToDeleting(List<int> array)
+        {
+            var result = new List<DeletingFromArray>();
+            array.RemoveAll(x => x % array[array.Count - 1] != 0);
+            return result;
+        }
+    }
+}
---
+++ lab6 final/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>() { 4, 3, 4, 5, -1, -2, -3, 2 };
+
+            Console.WriteLine("Исходный массив: ");
+
+            foreach (int item in array)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+
+            Console.WriteLine();
+
+            DeletingFromArray output = new DeletingFromArray();
+            List<DeletingFromArray> deleting = output.ToDeleting(array: array);
+
+            Console.WriteLine();
+            Console.WriteLine("Результат: ");
+            foreach (int item in array)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
