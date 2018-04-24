7ebdb39d65f16419acbff5180ab567d96d8f186e
Anastasia <simyeness@gmail.com>
2018-04-25 00:00:30 +0600

    lab8 final

---
+++ lab8 final/Lab8/Lab8/DeletingColumn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Lab8
+{
+    class DeletingColumn
+    {
+        private int ToFindElement(List<List<int>> array)
+        {
+            int indexOfmin = 0;
+            int min = array[0][0];
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array.Count; j++)
+                {
+                    if (array[i][j] < min)
+                    {
+                        min = array[i][j];
+                        indexOfmin = j;
+                    }
+                }
+            }
+            return indexOfmin;
+        }
+
+        public List<List<int>> ToDeleteCol(List<List<int>> array)
+        {
+            int j = ToFindElement(array);
+            for (int i = 0; i < array.Count; i++)
+            {
+                array[i].RemoveAt(j);
+            }
+            return array;
+        }
+    }
+}
---
+++ lab8 final/Lab8/Lab8/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Удалить столбец, содержащий минимальное отрицательное. */
+
+using System;
+using System.Collections.Generic;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            List<List<int>> array = new List<List<int>>();
+            Random rand = new Random();
+            Console.Write("Исходный массив: \n");
+
+            array.Add(new List<int>() { -10, 5, -1, 8 });
+            array.Add(new List<int>() { 3, -21, 12, -7 });
+            array.Add(new List<int>() { 7, 9, -2, 0 });
+            array.Add(new List<int>() { -3, 8, -15, 4 });
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array[i].Count; j++)
+                {
+                    Console.Write(array[i][j] + "  \t");
+                }
+                Console.WriteLine();
+            }
+
+            DeletingColumn del = new DeletingColumn();
+
+            List<List<int>> result = del.ToDeleteCol(array);
+
+            Console.Write("\nИтоговый массив: \n");
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array[i].Count; j++)
+                {
+                    Console.Write(array[i][j] + "  \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
