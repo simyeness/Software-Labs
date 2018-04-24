e5cb3d1cf3953ac3fdce146df21112776f02e073
Anastasia <simyeness@gmail.com>
2018-04-24 22:39:55 +0600

    lab9 final version

---
+++ lab9 final/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Генерируемый начальный массив: ");
+            Random rand = new Random();
+            int n = 3;
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = rand.Next(-10, 10);
+                    Console.Write(String.Format("{0,3} ", array[i, j]));
+                }
+                Console.WriteLine("\t");
+            }
+
+            Console.WriteLine();
+
+            Streamlining scrambling = new Streamlining();
+            int[,] result = scrambling.Replacement(array);
+            Console.WriteLine("Массив после перестановки: ");
+            for (int i = 0; i < result.GetLength(1); i++)
+            {
+                for (int j = 0; j < result.GetLength(0); j++)
+                {
+                    Console.Write(String.Format("{0,3} ", result[i, j]));
+                }
+                Console.WriteLine("\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab9 final/ConsoleApp1/ConsoleApp1/Streamlining.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    class Streamlining
+    { 
+  
+        public int[,] Replacement(int[,] array)
+        {
+            int element = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int x = 0; x < array.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < array.GetLength(1); y++)
+                        {
+                            if (array[i, j] > array[x, y])
+                            {
+                                element = array[i, j];
+                                array[i, j] = array[x, y];
+                                array[x, y] = element;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
