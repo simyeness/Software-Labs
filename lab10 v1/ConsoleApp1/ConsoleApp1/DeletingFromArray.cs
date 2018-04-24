e66a2bb635d8c2cb64b8b1ceea7c3e7e1dc52a5a
Anastasia <simyeness@gmail.com>
2018-04-24 22:14:16 +0600

    lab10 version 1

---
+++ lab10 v1/ConsoleApp1/ConsoleApp1/DeletingFromArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class DeletingFromArray
+    {
+        private List<int> array;
+
+        public DeletingFromArray(List<int> array)
+        {
+            this.array = array;
+        }
+
+        public List<int> DeleteToArray(List<int> array)
+        {
+            List<int> result = new List<int>(array);
+            array.RemoveAll(x => x % array[array.Count-1] != 0);
+            return result;
+        }
+    }
+}
