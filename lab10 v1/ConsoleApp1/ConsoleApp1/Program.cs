01f7cbfc271644f08c0209c2d52aaaad0038d2c3
Anastasia <simyeness@gmail.com>
2018-04-24 22:08:45 +0600

    Revert "Revert "lab10 version 1""
    This reverts commit 0b1ae8a9251c242e99046c936628c0968257a2c5.

---
+++ lab10 v1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line;
+            StreamReader Reader = new StreamReader("...\\Elements.txt");
+            List<int> array = new List<int>();
+            while ((line = Reader.ReadLine()) != null)
+            {
+                array.Add(Convert.ToInt32(line));
+            }
+
+            DeletingFromArray delete = new DeletingFromArray();
+
+            StreamWriter Writer = new StreamWriter("...\\Result.txt");
+            foreach (int i in delete.DeleteToArray(array))
+            {
+                Writer.Write(i + " ");
+            }
+            Writer.Close();
+            
+        }
+    }
+}
